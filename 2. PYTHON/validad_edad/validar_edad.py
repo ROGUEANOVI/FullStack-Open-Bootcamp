b6229c3ea665c56c9b58c5ba7790da855650d472
ROGUEANOVI <ovidioromero66@gmail.com>
2022-07-20 17:48:56 -0500

    Se agrego el encabezado a los archivos .py

--- 2. PYTHON/validad_edad/validar_edad.py
+++ 2. PYTHON/validad_edad/validar_edad.py
@@ -1,3 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#----------------------------------------------------------------------------
+# Created By  : Ovidio Antonio Romero Guerrero
+# Created Date: 2002/07/20
+# version ='1.0'
+# ---------------------------------------------------------------------------
+""" Details about the module and for what purpose it was built for"""
+# ---------------------------------------------------------------------------
+# Imports Line 5
+# ---------------------------------------------------------------------------
+# from ... import ...
 edad=int(input("Ingrese la edad: "))
 if edad > 0 and edad <= 150:
     if edad >= 18:

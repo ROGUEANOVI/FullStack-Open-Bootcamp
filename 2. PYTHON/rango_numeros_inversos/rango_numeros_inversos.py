d8f6ca948a25c28052ad07c0ca68bb2bb0e4ca4f
ROGUEANOVI <ovidioromero66@gmail.com>
2022-07-21 15:54:08 -0500

    Agregando ejercicio rango_numeros_inversos

---
+++ 2. PYTHON/rango_numeros_inversos/rango_numeros_inversos.py
@@ -0,0 +1,15 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Created By  : Ovidio Antonio Romero Guerrero
+# Created Date: 2002/07/21
+# version ='1.0'
+# ---------------------------------------------------------------------------
+""" Programa que muestra los numeros del 1 al 100 en forma descendente"""
+# ---------------------------------------------------------------------------
+# Imports
+# ---------------------------------------------------------------------------
+
+
+for numero in reversed(range(1, 101)):
+    print(numero)

dbb08cdb74162644cf04c714e82bf60b9e091b47
ROGUEANOVI <ovidioromero66@gmail.com>
2022-07-21 15:26:18 -0500

    Correccion de indentacion

--- 2. PYTHON/rango_numeros_impares/rango_numeros_impares.py
+++ 2. PYTHON/rango_numeros_impares/rango_numeros_impares.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3
+# !/usr/bin/env python3
 # -*- coding: utf-8 -*-
-#----------------------------------------------------------------------------
+# ----------------------------------------------------------------------------
 # Created By  : Ovidio Antonio Romero Guerrero
 # Created Date: 2002/07/21
 # version ='1.0'
@@ -12,14 +12,12 @@
 numero_inicial = int(input("Ingrese el numero inicial: "))
 numero_final = int(input("Ingrese el numero final: "))
 lista_impares = []
-while(numero_final <= numero_inicial):
-  numero_final = int(input("El numero_final debe ser mayor al numero inicial por favor, introduce otro numero: "))
 
-for numero in range(numero_inicial,numero_final + 1):
-  if ((numero % 2) != 0):
-    lista_impares.append(numero)
+while numero_final <= numero_inicial:
+    numero_final = int(input("El numero_final debe ser mayor al numero inicial por favor, introduce otro numero: "))
+
+for numero in range(numero_inicial, numero_final + 1):
+    if numero % 2 != 0:
+        lista_impares.append(numero)
 
 print(lista_impares)
-
-
-

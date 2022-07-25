379991ec3a34c93027cee83d10574e9661a040fc
ROGUEANOVI <ovidioromero66@gmail.com>
2022-07-25 13:28:36 -0500

    Agregando ejercicio funcion_calcular_bisiesto.py

---
+++ 2. PYTHON/funcion_calcular_bisiesto/funcion_calcular_bisiesto.py
@@ -0,0 +1,22 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ----------------------------------------------------------------------------
+# Created By  : Ovidio Antonio Romero Guerrero
+# Created Date: 2002/07/25
+# version ='1.0'
+# ---------------------------------------------------------------------------
+""" Programa que muestra si un año es bisiesto o no, mediante una funcioncd"""
+# ---------------------------------------------------------------------------
+# Imports
+# ---------------------------------------------------------------------------
+
+
+def es_bisiesto(anio):
+    if ((anio % 4) == 0) and ((anio % 100) != 0 or (anio % 400) == 0):
+        return f"{anio} es bisiesto"
+    else:
+        return f"{anio} NO es bisiesto"
+
+
+bisiesto = es_bisiesto(2015)
+print(bisiesto)

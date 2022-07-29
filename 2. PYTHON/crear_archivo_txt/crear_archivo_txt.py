8b154c5037be21cce78ee3822e4b9c7b377bc0d8
ROGUEANOVI <ovidioromero66@gmail.com>
2022-07-28 20:04:04 -0500

    Modificando archivo crear_archivo_txt.py

--- 2. PYTHON/crear_archivo_txt/crear_archivo_txt.py
+++ 2. PYTHON/crear_archivo_txt/crear_archivo_txt.py
@@ -11,10 +11,10 @@
 # ---------------------------------------------------------------------------
 
 archivo = open("E:/Documentos/4. CURSOS PROGRAMACION/5. OPEN BOOTCAMP/2. PYTHON/crear_archivo_txt/archivo.txt", "w")
-archivo.write("Primera linea del archivo\n")
+archivo.write("Primera linea del archivo.\n")
 archivo.close()
 
 archivo = open("archivo.txt", "r+")
 archivo.readline()
-archivo.write('Esta es la segunda vez que escribo.\n')
+archivo.write('Segunda linea del archivo.\n')
 archivo.close()
